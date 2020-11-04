b111171360f78c5c04aa37078ff13340707bdf9e
M Fachri <mfachri@upi.edu>
2020-11-04 08:30:56 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+// import "./src/styles/bootstrap.min.css";
+// import "./src/styles/bootstrap-grid.min.css";
+import main from "./src/script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
